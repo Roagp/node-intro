2654e11d8e4fe4f1440916f23206f4635dbaf2b4
Rocío <rocio.c.martin@gmail.com>
2021-05-13 18:07:36 +0200

    ejercicios 1 y 2

--- ejercicios/1/index.js
+++ ejercicios/1/index.js
@@ -1,1 +1,6 @@
 console.log("Primer ejercicio");
+const fs = require("fs");
+const pkg = fs.readFileSync("input.txt");
+const transformedText = pkg.toString("utf-8").toUpperCase();
+
+fs.writeFileSync("./output.txt", transformedText);
